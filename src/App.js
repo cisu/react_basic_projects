283573a12374ace40c29d94ab124e07239e2554a
george <geor@webdi.com>
2021-05-24 23:47:42 +0300

    02-tour done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !==id);
+
+    setTours(newTours);
+  }
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -30,9 +37,20 @@ function App() {
   if (loading) {
     return <main><Loading /></main>;
   }
+
+  if(tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className='btn' onClick={fetchTours}>
+          refresh
+        </button>
+      </div>
+    </main>
+  }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
   );
 }
