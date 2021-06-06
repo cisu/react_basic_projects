c6581c9b4b0f1d3f0a3d892706b78e9d06accc0d
george <geor@webdi.com>
2021-06-06 22:52:42 +0300

    add all Button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [categories, setCategories] = useState([]);
 
   const filterItems = category => {
+
+    if(category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
     const newItems = items.filter(item => item.category === category);
     setMenuItems(newItems);
   };
--- src/Categories.js
+++ src/Categories.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 const Categories = ({filterItems}) => {
   return <div className='btn-container'>
+
+  {/* all  */}
+   <button 
+    className='filter-btn' 
+    onClick={() => filterItems('all')}
+    >
+      all
+   </button>
+
+    {/* breakfast */}
     <button 
     className='filter-btn' 
     onClick={() => filterItems('breakfast')}
     >
       breakfast
    </button>
+ 
     </div>;
 };
 
