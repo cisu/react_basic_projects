5917029254eb545cd0567b33355a2a1d37cfa11d
george <geor@webdi.com>
2021-06-10 15:44:22 +0300

    setup color-generator

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
-import React, {useState} from 'react';
-// because we export default we can call want ever we want in this case form text we call this data
-import data from './data';
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState([]);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-  
-    let amount = parseInt(count)
-    console.log(typeof count);
-
-    if(count <=0){
-      amount = 1
-    }
-    if(count> 8){
-      amount = 8
-    }
-    setText(data.slice(0,amount));
-  };
-
-  return (
-    <section className='section-center'>
-      <h3>tired of boring lorem ipsum?</h3>
-      <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>paragraphs:</label>
-        <input
-          type='number'
-          name='amount'
-          id='amount'
-          value={count}
-          onChange={e => setCount(e.target.value)}
-        />
-        <button type='submit' className='btn'>
-          generate
-        </button>
-      </form>
-      <article className='lorem-text'>
-        {text.map((item, index) => {
-          return <p key={index}>{item}</p>;
-        })}
-      </article>
-    </section>
-  );
+  return <h2>color generator project</h2>
 }
 
-export default App;
+export default App
