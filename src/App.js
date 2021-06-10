138743ab3090c0da2396a6f0031a0a75d7283bad
george <geor@webdi.com>
2021-06-10 15:27:16 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,73 +1,35 @@
-import React, {useState, useEffect} from 'react';
-import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
-import {FaQuoteRight} from 'react-icons/fa';
+import React, {useState} from 'react';
+// because we export default we can call want ever we want in this case form text we call this data
 import data from './data';
 
 function App() {
-  const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState(0);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-  useEffect (() => {
-    const lastIndex = people.length -1 ;
-    if(index < 0) {
-      setIndex(lastIndex)
-    }
-    if (index > lastIndex){
-      setIndex(0)
-    }
-  }, [index, people]);
-
-  useEffect(() => {
-   let slider = setInterval(() => {
-      setIndex(index + 1)
-    },3000);
-
-    return () => clearInterval(slider);
-
-  }, [index]);
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('hello world');
+  };
 
   return (
-    <section className='section'>
-      <div className='title'>
-        <h2>
-          <span>/</span> reviews
-        </h2>
-      </div>
-      <div className='section-center'>
-        {people.map((person, personIndex) => {
-          const {id, image, name, title, quote} = person;
-          // more stuff coming up
-          let position = 'nextSlide';
-
-          if (personIndex === index) {
-            position = 'activeSlide';
-          }
-
-          if (
-            personIndex === index - 1 ||
-            (index === 0 && personIndex === people.length - 1)
-          ) {
-            position = 'lastSlide';
-          }
-
-          return (
-            <article className={position} key={id}>
-              <img src={image} alt={name} className='person-img' />
-              <h4>{name}</h4>
-              <p className='title'>{title}</p>
-              <p className='text'>{quote}</p>
-              <FaQuoteRight className='icon' />
-            </article>
-          );
-        })}
-        <button className='prev' onClick={() => setIndex(index - 1)}>
-          <FiChevronLeft />
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
         </button>
-
-        <button className='next' onClick={() => setIndex(index + 1)}>
-          <FiChevronRight />
-        </button>
-      </div>
+      </form>
+      <article className='lorem-text'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, magnam?</p>
+      </article>
     </section>
   );
 }
