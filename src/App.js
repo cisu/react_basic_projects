489bba0e865c083530eca18daa721c173b11a44f
george <geor@webdi.com>
2021-06-12 13:14:20 +0300

    setup grocery-dub

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
-import React, {useState} from 'react';
-import SingleColor from './SingleColor';
-
-import Values from 'values.js';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
-  const [color, setColor] = useState('');
-  const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values('#222222').all(10));
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log('hello');
-
-    try {
-      let colors = new Values(color).all(10);
-      setList(colors);
-      // console.log(colors)
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
-  };
-
-  return (
-    <>
-      <section className='container'>
-        <h3>color generator</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            value={color}
-            placeholder='#f15025'
-            onChange={e => setColor(e.target.value)}
-            className={`${error ? 'error' : null}`}
-          />
-          <button className='btn' type='submit'>
-            submit
-          </button>
-        </form>
-      </section>
-      <section className='colors'>
-        {list.map((color, index) => {
-          console.log(color);
-          return (
-            <SingleColor
-              key={index}
-              {...color}
-              index={index}
-            />
-          );
-        })}
-      </section>
-    </>
-  );
+  return <h2>grocery bud setup</h2>
 }
 
-export default App;
+export default App
