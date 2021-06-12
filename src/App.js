779428a295dc55e64e51f7906db91c7392a0d2fa
george <geor@webdi.com>
2021-06-12 14:10:58 +0300

    alert

--- src/App.js
+++ src/App.js
@@ -7,38 +7,58 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log('hello');
+    if (!name) {
+      // display alert
+      // setAlert({show:true, mgs: 'please enter value', type: 'danger'})
+      showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(), title: name};
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({show, type, msg});
   };
 
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3>grocery bud</h3>
-        <div className="form-control">
-          <input 
-          type="text" 
-          className="grocery" 
-          placeholder='e.g. eggs' 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={e => setName(e.target.value)}
           />
-          <button type="submit" className="submit-btn">
+          <button type='submit' className='submit-btn'>
             {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
+      {list.length > 0 && (
         <div className='grocery-container'>
-          <List />
+          <List items={list} />
           <button className='clear-btn' onClick>
             clear items
           </button>
         </div>
-      
+      )}
     </section>
   );
 }
