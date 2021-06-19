969c88e7e1ad83e2eae8088a6a29d1ed4ce237b7
george <geor@webdi.com>
2021-06-19 22:53:34 +0300

    setutp project

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,47 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import {FaTimes} from 'react-icons/fa';
-import {social, links} from './data';
-import {useGlobalContext} from './context';
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import sublinks from './data'
 
 const Sidebar = () => {
-  const {isSidebarOpen, closeSidebar} = useGlobalContext();
+  return <h2>sidebar component</h2>
+}
 
-  return (
-    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'} `}>
-      <div className='sidebar-header'>
-        {/* <img src={logo} className="logo" alt="george" /> */}
-        <h2 className='logo'>Cisu</h2>
-        <button className='close-btn' onClick={closeSidebar}>
-          <FaTimes />
-        </button>
-      </div>
-      <ul className='links'>
-        {links.map(link => {
-          const {id, url, text, icon} = link;
-          return (
-            <li key={id}>
-              <a href={url}>
-                {icon}
-                {text}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <ul className='social-icons'>
-        {social.map(link => {
-          const {id, url, icon} = link;
-
-          return (
-            <li key={id}>
-              <a href={url}>{icon}</a>
-            </li>
-          );
-        })}
-      </ul>
-    </aside>
-  );
-};
-
-export default Sidebar;
+export default Sidebar
