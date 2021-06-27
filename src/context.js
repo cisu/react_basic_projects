466fe4e46b4aec1aa5d4bceb2dc69c9c9315511f
george <geor@webdi.com>
2021-06-27 23:30:50 +0300

    Fetch Data

--- src/context.js
+++ src/context.js
@@ -17,24 +17,37 @@ const AppProvider = ({children}) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const clearCart = () => {
-    dispatch({type: 'CLEAR_CART'})
-  }
+    dispatch({type: 'CLEAR_CART'});
+  };
 
-  const remove = (id) => {
-     dispatch({type: "REMOVE", payload: id})
-  }
+  const remove = id => {
+    dispatch({type: 'REMOVE', payload: id});
+  };
 
-  const increase = (id) => {
-    dispatch({type: "INCREASE", payload: id})
-  }
+  const increase = id => {
+    dispatch({type: 'INCREASE', payload: id});
+  };
 
-  const decrease = (id) => {
-    dispatch({type: "DECREASE", payload: id})
-  }
+  const decrease = id => {
+    dispatch({type: 'DECREASE', payload: id});
+  };
+
+  const fetchData = async () => {
+    dispatch({type: 'LOADING'});
+    const response = await fetch(url);
+    const cart = await response.json();
+
+    dispatch({type: 'DISPLAY_ITEMS', payload: cart});
+  };
+
+
+  useEffect(()=> {
+    fetchData()
+  }, [])
 
   useEffect(() => {
-    dispatch({type: 'GET_TOTALS'})
-  }, [state.cart])
+    dispatch({type: 'GET_TOTALS'});
+  }, [state.cart]);
 
   return (
     <AppContext.Provider
@@ -43,7 +56,7 @@ const AppProvider = ({children}) => {
         clearCart,
         remove,
         increase,
-        decrease
+        decrease,
       }}
     >
       {children}
