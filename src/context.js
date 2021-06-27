74252363db1302f828a68cc904c29688a81569db
george <geor@webdi.com>
2021-06-27 21:34:11 +0300

    links

--- src/context.js
+++ src/context.js
@@ -4,10 +4,12 @@ import sublinks from './data';
 const AppContext = React.createContext();
 
 export const AppProvider = ({children}) => {
-  const [ isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
 
-  const [location, setLocation] = useState({})
+  const [location, setLocation] = useState({});
+
+  const [page, setPage] = useState({page: '', links: []});
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -18,7 +20,9 @@ export const AppProvider = ({children}) => {
   };
 
   const openSubmenu = (text, coordinates) => {
-    setLocation(coordinates)
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
 
@@ -35,7 +39,8 @@ export const AppProvider = ({children}) => {
         openSidebar,
         closeSubmenu,
         closeSidebar,
-        location
+        location,
+        page
       }}
     >
       {children}
@@ -44,6 +49,5 @@ export const AppProvider = ({children}) => {
 };
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
+  return useContext(AppContext);
 };
-
