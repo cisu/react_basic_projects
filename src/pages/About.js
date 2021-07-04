a51bb7acd3832d3cb909a8bf7e145248bb17c6db
george <geor@webdi.com>
2021-07-04 16:07:59 +0300

    about $ error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo culpa
+        quaerat eius dicta dignissimos optio necessitatibus ab blanditiis,
+        aliquid veniam id delectus dolore perspiciatis adipisci magni
+        reprehenderit repudiandae hic itaque. Nostrum, hic sint aut consectetur
+        nam iste reprehenderit odit nulla?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
